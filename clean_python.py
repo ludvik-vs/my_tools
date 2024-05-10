298bab2d7bed167ec31abfda61fd3fa9acf6c736
J. Ludvik VS <joseluis18vs@gmail.com>
2024-05-10 13:22:42 -0600

    Update clean_python.py
    fix dir err

--- clean_python.py
+++ clean_python.py
@@ -1,11 +1,17 @@
+import os
 import subprocess
-import re
+
+os.listdir()
 
 with open('requirements.txt', 'r') as file:
     packages = [line.strip() for line in file]
 
-for package in packages:
-    subprocess.call(['pip', 'uninstall', '-y', package])
-
+for packageName in packages:
+    try:
+        subprocess.call(f'echo remove {packageName}', shell=True)
+        subprocess.call('pip3.11 uninstall -y ' + packageName, shell=True)
+    except FileNotFoundError as e:
+        print(e)
+        print("\n")
 
 # pip freeze > requirements.txt
